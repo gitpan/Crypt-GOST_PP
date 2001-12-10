a7d6a51dd984d05c99a1d7e2c6c1b6e5e35f4e85
Kurt Kincaid <sifukurt@yahoo.com>
2001-12-10 20:02:18 +0000

    Import of SIFUKURT/Crypt-GOST_PP-1.0 from CPAN.
    gitpan-cpan-distribution: Crypt-GOST_PP
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         SIFUKURT/Crypt-GOST_PP-1.0.tar.gz
    gitpan-cpan-author:       SIFUKURT
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,31 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+BEGIN { plan tests => 1 };
+use Crypt::GOST_PP;
+ok(1); # If we made it this far, we're ok.
+
+#########################
+
+$text = "This is an encryption test use Crypt::GOST_PP";
+$pp   = "passphrase";
+$ref  = Crypt::GOST_PP->new( $pp );
+$enc  = $ref->encrypt( $text );
+$ref2 = Crypt::GOST_PP->new( $pp );
+$dec  = $ref2->decrypt( $enc );
+
+print <<END;
+Original Text:  $text
+-----
+Passphrase:     $pp
+-----
+Cipher Text:    $enc
+-----
+Decrypted Text: $dec
+END
+
